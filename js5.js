558f071da31de7e7f695470430b5cbecd8c0b8b9
Jonatan Javier Garcia Reyna <jonyjony19955@hotmail.com>
2022-10-22 18:34:48 -0500

    Función terminada

--- js5.js
+++ js5.js
@@ -1,24 +1,24 @@
 //Calculadora
-
-function calculator(numero1,numero2) {
-    let opDoc = document.getElementById("tipo");
-    if (opDoc == "op1") {
+function calculator() {
+    let resultado = 0;
+    let opDoc = parseInt(document.getElementById("tipo").value);
+    let numero1 = parseInt(document.getElementById("num1").value);
+    let numero2 = parseInt(document.getElementById("num2").value);
+    if (opDoc == 1) {
         resultado = numero1 + numero2;
+        console.log("Resultado: ",resultado)
         return resultado;
     }
-    if (opDoc == "op2") {
+    if (opDoc == 2) {
         resultado = numero1 - numero2;
         return resultado;
     }
-    if (opDoc == "op3") {
+    if (opDoc == 2) {
         resultado = numero1 * numero2;
         return resultado;
     }
-    if (opDoc == "op4") {
+    if (opDoc == 2) {
         resultado = numero1 / numero2;
         return resultado;
     }
-
-    console.log("Resultado:" ,resultado)
 }
-
