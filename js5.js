ad44cde6d90e7eea8a6ad59a7d9f437418f9a1ea
Jonatan Javier Garcia Reyna <jonyjony19955@hotmail.com>
2022-10-23 00:18:35 -0500

    Se trabaja en el print del resultado

--- js5.js
+++ js5.js
@@ -6,7 +6,6 @@ function calculator() {
     let numero2 = parseInt(document.getElementById("num2").value);
     if (opDoc == 1) {
         resultado = numero1 + numero2;
-        console.log("Resultado: ",resultado)
         return resultado;
     }
     if (opDoc == 2) {
