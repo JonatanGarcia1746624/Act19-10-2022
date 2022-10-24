f1f56cc5bc91db866f4195ac1bd25aff29efa042
Jonatan Javier Garcia Reyna <jonyjony19955@hotmail.com>
2022-10-23 20:28:53 -0500

    Se trabaja en el archivo de imagen

--- js/Calculadora.js
+++ js/Calculadora.js
@@ -4,7 +4,6 @@ function calculator() {
     let opDoc = parseInt(document.getElementById("tipo").value);
     let numero1 = parseInt(document.getElementById("num1").value);
     let numero2 = parseInt(document.getElementById("num2").value);
-    let res = toString;
     if (opDoc == 1) {
         resultado = numero1 + numero2;
         document.getElementById("resultado").innerHTML = resultado;
---
+++ js/VerImagen.js
@@ -0,0 +1,8 @@
+//Función Ver Imagen
+function verImagen() {
+    let opDoc = parseInt(document.getElementById("tipo").value);
+    let imgFuente = toString();
+    if (opDoc == 1) {
+        
+    }
+}
