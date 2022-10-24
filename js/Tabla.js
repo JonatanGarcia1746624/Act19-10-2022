0b9a3ca9b5ef84c9f95ca32f1688c33abba1b906
Jonatan Javier Garcia Reyna <jonyjony19955@hotmail.com>
2022-10-24 14:02:42 -0500

    Tabla terminada

--- js/Tabla.js
+++ js/Tabla.js
@@ -1,9 +1,11 @@
 function verTabla() {
     let num = parseInt(document.getElementById("num").value);
     let numT = 0;
+    let indice = 0;
     for (let index =1; index < 11; index++) {
         numT++;
-        let resul = numT + ' X ' + num + ' = ' + numT * num;
-        document.getElementById("resultado1").innerText = (resul);
+        let resul = num + ' X ' + numT + ' = ' + numT * num;
+        indice++;
+        document.getElementById("resultado"+indice).innerText = (resul);
     }
 }
